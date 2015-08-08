31cc986c38842446827d83a43da64ab24f0be9c8
Roqueirow <brunoteo@msn.com>
2015-08-08 16:18:07 -0300

    Implementando funcionalidades da classe MapaDAO
    Testes de MapaDAOTest validam o funcionamento.

--- hiring-exam/src/main/java/br/com/programadoremjava/s2/dao/MapaDAO.java
+++ hiring-exam/src/main/java/br/com/programadoremjava/s2/dao/MapaDAO.java
@@ -1,21 +1,99 @@
 package br.com.programadoremjava.s2.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
+
+import br.com.programadoremjava.s2.db.EntityManagerFactory;
 import br.com.programadoremjava.s2.entity.Mapa;
 
+/**
+ * Classe para gerenciar os Mapas no DB
+ * 
+ * @author Bruno Teofilo da Silva
+ */
 public class MapaDAO {
 
-	static final Mapa MAPA = new Mapa();
-	
+	/**
+	 * Cria um novo mapa ou atualiza um mapa existente;
+	 * 
+	 * @param mapa
+	 *            {@link Mapa} a ser persistido no DB.
+	 * @return o {@link Mapa} atualizado após ser persistido.
+	 */
 	public Mapa put(Mapa mapa) {
-		return MAPA;
+		if (null == mapa.getId()) {
+			return save(mapa);
+		} else {
+			return update(mapa);
+		}
 	}
 
-	public Mapa getById(Long id){
-		return MAPA;
+	private Mapa update(Mapa mapa) {
+		EntityManager manager = EntityManagerFactory.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		manager.merge(mapa);
+		tx.commit();
+		return mapa;
 	}
-	
-	public Mapa getByNome(String nome) {
-		return MAPA;
+
+	private Mapa save(Mapa mapa) {
+		EntityManager manager = EntityManagerFactory.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		manager.persist(mapa);
+		tx.commit();
+		return mapa;
+	}
+
+	/**
+	 * Recupera um mapa utilizando o seu ID
+	 * 
+	 * @param id
+	 *            ID do mapa a ser recuperado do DB
+	 * @return o {@link Mapa} com o ID informado
+	 * @throws EntityNotFoundException
+	 *             caso o id não exista no DB.
+	 */
+	public Mapa getById(Long id) {
+		EntityManager manager = EntityManagerFactory.getEntityManager();
+		Mapa mapa = manager.find(Mapa.class, id);
+		if (null != mapa) {
+			return mapa;
+		} else {
+			throw new EntityNotFoundException(String.format("Não foi possível encontrar um mapa com o id '%d'.", id));
+		}
+
+	}
+
+	/**
+	 * Recupera um mapa utilizando o seu Nome
+	 * 
+	 * @param nome
+	 *            Nome do mapa a ser encontrado no DB.
+	 * @return o {@link Mapa} com o nome informado, ou <code>null</code> caso
+	 *         não exista.
+	 * @throws Pode
+	 *             lançar {@link NonUniqueResultException} se mais de uma mapa
+	 *             com o mesmo nome existir no DB.
+	 */
+	public Mapa findByNome(String nome) {
+		EntityManager manager = EntityManagerFactory.getEntityManager();
+		TypedQuery<Mapa> query = manager.createQuery("SELECT m FROM Mapa m WHERE m.nome=:nome", Mapa.class);
+		query.setParameter("nome", nome);
+		List<Mapa> resultList = query.getResultList();
+		if (resultList.size() == 0) {
+			return null;
+		} else if (resultList.size() == 1) {
+			return resultList.get(0);
+		} else {
+			throw new NonUniqueResultException();
+		}
 	}
 
 }
--- hiring-exam/src/test/java/br/com/programadoremjava/s2/dao/MapaDAOTest.java
+++ hiring-exam/src/test/java/br/com/programadoremjava/s2/dao/MapaDAOTest.java
@@ -1,12 +1,16 @@
 package br.com.programadoremjava.s2.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.junit.Test;
 
@@ -16,67 +20,157 @@ import br.com.programadoremjava.s2.entity.Segmento;
 
 public class MapaDAOTest {
 
-	private static final EntityManager MANAGER = EntityManagerFactory.getEntityManager();
-	private static final MapaDAO DAO = new MapaDAO();
-
 	@Test
 	public void testPut() {
-		Mapa mapa1 = new Mapa();
-		mapa1.setNome("São Paulo");
+
+		// Instanciando a DAO e o EntityManager
+		final MapaDAO DAO = new MapaDAO();
+		final EntityManager MANAGER = EntityManagerFactory.getEntityManager();
+
+		// Criando um mapa para ser persistido utilizando a DAO
+		Mapa mapa = new Mapa();
+		mapa.setNome("São Paulo");
 
 		Segmento segmento1 = new Segmento();
-		segmento1.setMapa(mapa1);
+		segmento1.setMapa(mapa);
 		segmento1.setOrigem("São Paulo");
 		segmento1.setDestino("Santos");
 		segmento1.setDistancia(72l);
-		mapa1.getSegmentos().add(segmento1);
+		mapa.getSegmentos().add(segmento1);
 
 		Segmento segmento2 = new Segmento();
-		segmento2.setMapa(mapa1);
+		segmento2.setMapa(mapa);
 		segmento2.setOrigem("São Paulo");
 		segmento2.setDestino("Araraquara");
 		segmento2.setDistancia(278l);
-		mapa1.getSegmentos().add(segmento2);
+		mapa.getSegmentos().add(segmento2);
 
 		Segmento segmento3 = new Segmento();
-		segmento3.setMapa(mapa1);
+		segmento3.setMapa(mapa);
 		segmento3.setOrigem("São Paulo");
 		segmento3.setDestino("São Sebastião");
 		segmento3.setDistancia(198l);
-		mapa1.getSegmentos().add(segmento3);
+		mapa.getSegmentos().add(segmento3);
 
 		Segmento segmento4 = new Segmento();
-		segmento4.setMapa(mapa1);
+		segmento4.setMapa(mapa);
 		segmento4.setOrigem("Santos");
 		segmento4.setDestino("São Sebastião");
 		segmento4.setDistancia(160l);
-		mapa1.getSegmentos().add(segmento4);
+		mapa.getSegmentos().add(segmento4);
 
-		DAO.put(mapa1);
+		// Chamando a DAO para fazer a persistência
+		DAO.put(mapa);
+		assertNotNull(mapa.getId());
 
-		List<Mapa> resultList = MANAGER.createQuery("SELECT m FROM Mapa m", Mapa.class).getResultList();
-
+		// Recuperando e testando se o objeto foi persistido
+		TypedQuery<Mapa> query = MANAGER.createQuery("SELECT m FROM Mapa m WHERE m.id=:id", Mapa.class);
+		query.setParameter("id", mapa.getId());
+		List<Mapa> resultList = query.getResultList();
 		assertEquals(1, resultList.size());
+
+		Mapa mapaFromDB = resultList.get(0);
+		assertEquals("São Paulo", mapaFromDB.getNome());
+		assertEquals(4, mapaFromDB.getSegmentos().size());
+		assertEquals("São Paulo", mapaFromDB.getSegmentos().get(0).getOrigem());
+		assertEquals("Santos", mapaFromDB.getSegmentos().get(0).getDestino());
+		assertEquals(72l, mapaFromDB.getSegmentos().get(0).getDistancia().longValue());
+
+		// Atualizando o mapa recuperado do DB para verificar se a DAO o
+		// atualiza
+		Segmento segmento5 = new Segmento();
+		segmento5.setMapa(mapa);
+		segmento5.setOrigem("Santos");
+		segmento5.setDestino("Ubatuba");
+		segmento5.setDistancia(232l);
+		mapaFromDB.getSegmentos().add(segmento5);
+		mapaFromDB.setNome("SP");
+
+		// Chamando a DAO para atualizar o DB
+		DAO.put(mapaFromDB);
+		assertEquals(mapa.getId(), mapaFromDB.getId());
+
+		// Confirmando que não foi criado um novo mapa no DB
+		TypedQuery<Mapa> updatedQuery = MANAGER.createQuery("SELECT m FROM Mapa m WHERE m.id=:id", Mapa.class);
+		updatedQuery.setParameter("id", mapa.getId());
+		List<Mapa> updatedResultList = updatedQuery.getResultList();
+		assertEquals(1, updatedResultList.size());
+
+		// Confirmando que o mapa realmente foi atualizado.
+		Mapa updatedMapa = updatedResultList.get(0);
+		assertEquals("SP", updatedMapa.getNome());
+		assertEquals(5, updatedMapa.getSegmentos().size());
+
 	}
 
-	@Test
+	@Test(expected = EntityNotFoundException.class)
 	public void testGetById() {
-		Query query = MANAGER.createNativeQuery("INSERT INTO Mapa (mapa_id, nome) VALUES (1,'São Paulo')");
+		// Instanciando a DAO e o EntityManager
+		final MapaDAO DAO = new MapaDAO();
+		final EntityManager MANAGER = EntityManagerFactory.getEntityManager();
+
+		// Criando um mapa no DB
+		Query query = MANAGER.createNativeQuery("INSERT INTO Mapa (mapa_id, nome) VALUES (13579,'São Paulo')");
 		EntityTransaction tx = MANAGER.getTransaction();
 		tx.begin();
 		query.executeUpdate();
 		tx.commit();
-		assertEquals(1l, DAO.getById(1l).getId().longValue());
+		// Criando alguns seguimentos no mapa
+		query = MANAGER.createNativeQuery("INSERT INTO Segmento (mapa_id, origem, destino, distancia) VALUES " //
+				+ "(13579,'São Paulo','Santos',72)," //
+				+ "(13579,'São Paulo','Araraquara',278)," //
+				+ "(13579,'São Paulo','São Sebastião',198)," //
+				+ "(13579,'Santos','São Sebastião',160)");
+		tx = MANAGER.getTransaction();
+		tx.begin();
+		query.executeUpdate();
+		tx.commit();
+
+		// Recuperando o mapa criando usando a DAO
+		Mapa mapa = DAO.getById(13579l);
+		assertNotNull(mapa);
+		assertEquals(13579l, mapa.getId().longValue());
+		assertEquals("São Paulo", mapa.getNome());
+		assertEquals(4, mapa.getSegmentos().size());
+
+		// Tentando recuperar um mapa que não existe
+		DAO.getById(24680l);
 	}
 
 	@Test
-	public void testGetByNome() {
-		Query query = MANAGER.createNativeQuery("INSERT INTO Mapa (mapa_id, nome) VALUES (1,'São Paulo')");
+	public void testFindByNome() {
+		// Instanciando a DAO e o EntityManager
+		final MapaDAO DAO = new MapaDAO();
+		final EntityManager MANAGER = EntityManagerFactory.getEntityManager();
+
+		// Criando um mapa no DB
+		Query query = MANAGER.createNativeQuery("INSERT INTO Mapa (mapa_id, nome) VALUES (97531,'S.Paulo')");
 		EntityTransaction tx = MANAGER.getTransaction();
 		tx.begin();
 		query.executeUpdate();
 		tx.commit();
-		assertEquals(1l, DAO.getByNome("São Paulo").getId().longValue());
+		// Criando alguns seguimentos no mapa
+		query = MANAGER.createNativeQuery("INSERT INTO Segmento (mapa_id, origem, destino, distancia) VALUES " //
+				+ "(97531,'São Paulo','Santos',72)," //
+				+ "(97531,'São Paulo','Araraquara',278)," //
+				+ "(97531,'São Paulo','São Sebastião',198)," //
+				+ "(97531,'Santos','São Sebastião',160)");
+		tx = MANAGER.getTransaction();
+		tx.begin();
+		query.executeUpdate();
+		tx.commit();
+
+		// Procurando o mapa criando usando a DAO
+		Mapa mapa = DAO.findByNome("S.Paulo");
+		assertNotNull(mapa);
+		assertEquals(97531l, mapa.getId().longValue());
+		assertEquals("S.Paulo", mapa.getNome());
+		assertEquals(4, mapa.getSegmentos().size());
+
+		// Procurando um mapa que não existe
+		Mapa mapaNaoExistente = DAO.findByNome("Rio de Janeiro");
+		assertNull(mapaNaoExistente);
+
 	}
 
 }
